203caddd12611fe9271687d093979ccaeb35b00a
Devin R. Clark <dclark@wdtinc.com>
2017-06-20 14:27:46 -0500

    mix es6 modules and require

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-var qs = require('query-string');
+import qs from 'query-string';
+var catNames = require('cat-names');
+
+console.log(catNames.random());
 
 console.log(qs.stringify({
   tacos: true,
